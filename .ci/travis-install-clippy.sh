0ffbc47e3120442d81d337a5f31a6215d9f379c3
qzed <qzed@users.noreply.github.com>
2019-04-17 18:27:02 +0200

    CI: Update nightly version

--- .ci/travis-install-clippy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-if [ "$(cargo +$NIGHTLY clippy -- --version)" != "$CLIPPY" ] ; then
-    echo "Installing clippy '$CLIPPY' for rust nightly '$NIGHTLY'"
-    cargo +$NIGHTLY install clippy --force --vers $CLIPPY
-else
-    echo "Using cached clippy '$CLIPPY' for rust nightly '$NIGHTLY'"
-fi
